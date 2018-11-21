42ef67c7729a34bfd768589924028fa63b7bda33
Andrea Censi <acensi@ethz.ch>
2018-11-21 10:34:52 +0100

    error

--- lib-foobar/src/duckietown_foobar_tests/test1.py
+++ lib-foobar/src/duckietown_foobar_tests/test1.py
@@ -5,6 +5,7 @@ from comptests import comptest, run_module_tests, comptest_fails
 
 @comptest
 def test_sum1():
+    print('trying to check fp math')
     assert 0.1 + 0.2 != 0.3
 
 
@@ -12,6 +13,9 @@ def test_sum1():
 def test_sum2():
     np.testing.assert_almost_equal(0.1 + 0.2, 0.3)
 
+@comptest
+def test_to_fail():
+    assert 2 == 1
 
 # use comptest_fails for a test that is supposed to fail
 @comptest_fails
